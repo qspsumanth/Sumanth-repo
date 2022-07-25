8de13a208e77ba329fa07cb72d7aa36a3159085e
qspsumanth <sumanth12897@gmail.com>
2022-07-25 12:55:48 +0530

    commit 1

---
+++ src/test/java/com/TYSS/Vtiger/Practice_03.java
@@ -0,0 +1,18 @@
+package com.TYSS.Vtiger;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Practice_03 {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.quit();
+		
+	}
+
+}
---
+++ src/test/java/com/TYSS/Vtiger/Practice_2.java
@@ -0,0 +1,17 @@
+package com.TYSS.Vtiger;
+
+import org.testng.annotations.Test;
+
+public class Practice_2 {
+
+		@Test
+		public void test() {
+			String url = System.getProperty("url");
+			String browser = System.getProperty("Browser");
+		    System.out.println(url);
+			System.out.println(browser);
+			
+		
+	}
+
+}
